c56ead8c2273d95afa353e8859553d0d0d1880d8
Billy Huang <10646003@ntub.edu.tw>
2021-09-05 05:27:52 +0800

    feat(store): use middleware to make a logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,27 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
 
 import news from '../reducers/news';
 import user from '../reducers/user';
 
-const store = createStore(combineReducers({ news, user }));
+// const logger = (store) => (next) => (action) => {
+//   console.log('此次執行: ', action);
+//   console.log('執行之前的 state: ', store.getState());
+
+//   const result = next(action);
+
+//   console.log('執行之後的 state: ', store.getState());
+//   return result;
+// };
+
+// const logger2 = (store) => (next) => (action) => {
+//   const result = next(action);
+//   console.log(result);
+// };
+
+const store = createStore(
+  combineReducers({ news, user }),
+  applyMiddleware(logger),
+);
 
 export default store;
