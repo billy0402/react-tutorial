f6f991ff10a341358a0fca899def58a2fe859788
Billy Huang <10646003@ntub.edu.tw>
2021-09-05 05:41:46 +0800

    feat(News): sync and async action creator

--- src/actions/news.js
+++ src/actions/news.js
@@ -1,2 +1,8 @@
-export const addNews = (news) => ({ type: 'ADD_NEWS', payload: { news } });
+export const addNews = (theNews) => {
+  let news = {};
+  setTimeout(() => {
+    news = theNews;
+  }, 1000);
+  return { type: 'ADD_NEWS', payload: { news } };
+};
 export const deleteNews = (id) => ({ type: 'DELETE_NEWS', payload: { id } });
